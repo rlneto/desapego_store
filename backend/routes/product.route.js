1132c923d23fd006fb4583c193db02208ecec0be
Rudolfo Lange Neto <rlneto@live.com>
2025-04-30 12:33:04 -0300

    Rotas adicionadas no router do express.

---
+++ backend/routes/product.route.js
@@ -0,0 +1,101 @@
+import express from "express";
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+	try {
+		const products = await Product.find({});
+		res.status(200).json({
+			success: true,
+			data: products,
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: error,
+		});
+	}
+});
+
+router.post("/", async (req, res) => {
+	const product = req.body;
+
+	if (!product.name || !product.price || !product.image) {
+		return res.status(400).json({
+			success: false,
+			message: "Favor preencher todos os campos.",
+		});
+	}
+
+	const newProduct = new Product(product);
+
+	try {
+		await newProduct.save();
+		res.status(201).json({
+			success: true,
+			data: newProduct,
+		});
+	} catch (error) {
+		console.error("Erro ao criar produto: ", error.message);
+		res.status(500).json({
+			success: false,
+			message: "Erro no servidor.",
+		});
+	}
+});
+
+router.put("/:id", async (req, res) => {
+	const { id } = req.params;
+
+	const product = req.body;
+	console.log("ID do produto: ", id);
+	console.log("Dados do produto: ", product);
+
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(400).json({
+			success: false,
+			message: "ID inválido.",
+		});
+	}
+
+	try {
+		const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+			new: true,
+			runValidators: true,
+		});
+		if (!updatedProduct) {
+			return res.status(404).json({
+				success: false,
+				message: "Produto não encontrado.",
+			});
+		}
+		res.status(200).json({
+			success: true,
+			data: updatedProduct,
+		});
+	} catch (error) {
+		console.error("Erro ao atualizar produto: ", error.message);
+		res.status(500).json({
+			success: false,
+			message: "Erro no servidor.",
+		});
+	}
+});
+
+router.delete("/:id", async (req, res) => {
+	const { id } = req.params;
+	try {
+		await Product.findByIdAndDelete(id);
+		res.status(200).json({
+			success: true,
+			message: "Produto excluído.",
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: `Erro na requisição: ${error}`,
+		});
+	}
+});
+
+export default router;
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import dotenv from "dotenv";
 import express from "express";
-import mongoose from "mongoose";
 import { connectDB } from "./config/db.js";
-import Product from "./models/product.model.js";
+
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
@@ -10,101 +10,14 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/api/products", async (req, res) => {
-	try {
-		const products = await Product.find({});
-		res.status(200).json({
-			success: true,
-			data: products,
-		});
-	} catch (error) {
-		res.status(500).json({
-			success: false,
-			message: error,
-		});
-	}
-});
+// Middleware de debugging
+// app.use((req, res, next) => {
+// 	console.log("Incoming headers:", req.headers);
+// 	console.log("Content-Type:", req.get("Content-Type"));
+// 	next();
+// });
 
-app.post("/api/products", async (req, res) => {
-	const product = req.body;
-
-	if (!product.name || !product.price || !product.image) {
-		return res.status(400).json({
-			success: false,
-			message: "Favor preencher todos os campos.",
-		});
-	}
-
-	const newProduct = new Product(product);
-
-	try {
-		await newProduct.save();
-		res.status(201).json({
-			success: true,
-			data: newProduct,
-		});
-	} catch (error) {
-		console.error("Erro ao criar produto: ", error.message);
-		res.status(500).json({
-			success: false,
-			message: "Erro no servidor.",
-		});
-	}
-});
-
-app.put("/api/products/:id", async (req, res) => {
-	const { id } = req.params;
-
-	const product = req.body;
-	console.log("ID do produto: ", id);
-	console.log("Dados do produto: ", product);
-
-	if (!mongoose.Types.ObjectId.isValid(id)) {
-		return res.status(400).json({
-			success: false,
-			message: "ID inválido.",
-		});
-	}
-
-	try {
-		const updatedProduct = await Product.findByIdAndUpdate(id, product, {
-			new: true,
-			runValidators: true,
-		});
-		if (!updatedProduct) {
-			return res.status(404).json({
-				success: false,
-				message: "Produto não encontrado.",
-			});
-		}
-		res.status(200).json({
-			success: true,
-			data: updatedProduct,
-		});
-	} catch (error) {
-		console.error("Erro ao atualizar produto: ", error.message);
-		res.status(500).json({
-			success: false,
-			message: "Erro no servidor.",
-		});
-	}
-});
-
-app.delete("/api/products/:id", async (req, res) => {
-	const { id } = req.params;
-	try {
-		await Product.findByIdAndDelete(id);
-		res.status(200).json({
-			success: true,
-			message: "Produto excluído.",
-		});
-	} catch (error) {
-		res.status(500).json({
-			success: false,
-			message: `Erro na requisição: ${error}`,
-		});
-	}
-});
+app.use("/api/products", productRoutes);
 
 const PORTA = 5000;
 
