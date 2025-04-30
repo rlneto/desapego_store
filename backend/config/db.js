efbf3656584a189fbac049f56ac631602416e668
Rudolfo Lange Neto <rlneto@live.com>
2025-04-30 12:17:54 -0300

    Issue: Não está atualizando no PUT endpoint.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
+	if (process.env.NODE_ENV === "test") {
+		return;
+	}
 	try {
 		const conn = await mongoose.connect(process.env.MONGO_URI);
 		console.log(`Conectado ao MongoDB: ${conn.connection.host}`);
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
 import { connectDB } from "./config/db.js";
 import Product from "./models/product.model.js";
 
@@ -8,6 +9,21 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+
+app.get("/api/products", async (req, res) => {
+	try {
+		const products = await Product.find({});
+		res.status(200).json({
+			success: true,
+			data: products,
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: error,
+		});
+	}
+});
 
 app.post("/api/products", async (req, res) => {
 	const product = req.body;
@@ -32,6 +48,60 @@ app.post("/api/products", async (req, res) => {
 		res.status(500).json({
 			success: false,
 			message: "Erro no servidor.",
+		});
+	}
+});
+
+app.put("/api/products/:id", async (req, res) => {
+	const { id } = req.params;
+
+	const product = req.body;
+	console.log("ID do produto: ", id);
+	console.log("Dados do produto: ", product);
+
+	if (!mongoose.Types.ObjectId.isValid(id)) {
+		return res.status(400).json({
+			success: false,
+			message: "ID inválido.",
+		});
+	}
+
+	try {
+		const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+			new: true,
+			runValidators: true,
+		});
+		if (!updatedProduct) {
+			return res.status(404).json({
+				success: false,
+				message: "Produto não encontrado.",
+			});
+		}
+		res.status(200).json({
+			success: true,
+			data: updatedProduct,
+		});
+	} catch (error) {
+		console.error("Erro ao atualizar produto: ", error.message);
+		res.status(500).json({
+			success: false,
+			message: "Erro no servidor.",
+		});
+	}
+});
+
+app.delete("/api/products/:id", async (req, res) => {
+	const { id } = req.params;
+	try {
+		await Product.findByIdAndDelete(id);
+		res.status(200).json({
+			success: true,
+			message: "Produto excluído.",
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: `Erro na requisição: ${error}`,
 		});
 	}
 });
